3e15443953ebe0d2f1cc05260cae6bd6156ba92a
Samuel <samusr@gmail.com>
2025-01-05 10:30:44 +0000

    First commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,20 @@
+import type { Route } from './+types/home'
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Music Streaming Concept' },
+    {
+      name: 'description',
+      content:
+        'Descubre canciones, artistas y playlists con nuestra aplicación de streaming de música. Explora géneros, busca tus canciones favoritas y sumérgete en el mundo de la música.',
+    },
+  ]
+}
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
